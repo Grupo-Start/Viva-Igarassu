5ae6f4849bdbb96807eea327803aca42439bd5ca
Bianca Gonçalves <87552973+bianzof@users.noreply.github.com>
2025-11-12 18:11:45 -0300

    Add files via upload

---
+++ BDVIVAIGARASSUSCRIPT.sql
@@ -0,0 +1,11 @@
+select * from usuarios;
+select * from empresa;
+select * from eventos;
+select * from enderecos;
+select * from pontos_turisticos;
+select * from figurinhas;
+select * from usuario_figurinhas;
+select * from resgates;
+select * from recompensas;
+
+
