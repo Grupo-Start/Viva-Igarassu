00780a6a0ccad49b9fcafcf483f0cec100125da1
Deivson Sena Teixeira dos Santos <dsenateixeiradossantos@gmail.com>
2025-12-16 21:37:13 -0300

    fix: corrige import de roles na rota de recompensas

--- routes/recompensasRoutes.js
+++ routes/recompensasRoutes.js
@@ -1,19 +1,20 @@
 import { Router } from "express";
 import recompensasController from "../controllers/recompensasController.js";
-import auth from "../middleware/auth.js";
-import roles from "../middleware/roles.js";
+import { auth } from "../middleware/auth.js";
+import { permitir } from "../middleware/roles.js";
 
 const router = Router();
 
 router.use(auth);
 
-// Somente empreendedor cria/edita/deleta
-router.post("/", roles(["empreendedor"]), recompensasController.create);
-router.put("/:id", roles(["empreendedor"]), recompensasController.update);
-router.delete("/:id", roles(["empreendedor"]), recompensasController.delete);
+router.post("/", permitir("empreendedor"), recompensasController.create);
+router.put("/:id", permitir("empreendedor"), recompensasController.update);
+router.delete("/:id", permitir("empreendedor"), recompensasController.delete);
 
-// Qualquer usuário autenticado pode listar
+
 router.get("/", recompensasController.getAll);
 router.get("/:id", recompensasController.getById);
 
 export default router;
+
+
